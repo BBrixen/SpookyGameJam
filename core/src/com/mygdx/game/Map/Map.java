75f3ead445b84ad922450d6ec70f874c4c2d548c
Noah <nqsmith02@gmail.com>
2021-11-02 18:33:12 -0700

    this is a test commit, the map is kinda added

--- core/src/com/mygdx/game/Map/Map.java
+++ core/src/com/mygdx/game/Map/Map.java
@@ -1,17 +1,29 @@
 package com.mygdx.game.Map;
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.lang.Math;
 
 public class Map {
-    public static void SMLMaker(String[] args) {
-        List<List<Character>> SML=new ArrayList<>();
-        for (int i = 0; i <1050 ; i++) {
-            List<Character> eachLineList= new ArrayList<Character>();
+    public List<List<Character>> SML;
+    public Map() {
+        SML = new ArrayList<>();
+        for (int i = 0; i < 1050; i++) {
+            List<Character> eachLineList = new ArrayList<Character>();
             for (int j = 0; j < 1050; j++) {
                 eachLineList.add('a');
             }
             SML.add(eachLineList);
         }
+        System.out.println(((Math.random() * 1049)));
+    }
+    public void virus(float spreadRate, float decayRate, char infectionType) {
+        Queue<Pair<Integer,Integer>> theCurrentQueue= new LinkedList<>();
+        System.out.println(((Math.random() * (1049 - 0)) + 1049));
     }
 }
+
